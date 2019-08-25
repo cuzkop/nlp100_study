c22eea50d42fc410f39ba88fb66aa0939014f3bd
kzk0829 <kzk0829kop@gmail.com>
2019-08-25 22:01:22 +0900

    add 3

--- nlp100_00-09.py
+++ nlp100_00-09.py
@@ -23,3 +23,22 @@ print(answer)
 
 # よりスマートな書き方
 print(''.join(s1 + s2 for s1, s2 in zip(str1,str2)))
+
+# 03. 円周率
+# "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."という文を単語に分解し，
+# 各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+
+str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+str = str.replace(",", "").replace(".", "")
+answer = []
+for s in str.split(" "):
+    answer.append(len(s))
+
+print(answer)
+
+# こっちの方がいいらしい
+
+s = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+
+import re
+list(len(m) for m in re.findall('[a-zA-Z]+', s))
