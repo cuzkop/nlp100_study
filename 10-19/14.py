3351dda9c1bfdc56c3bd9029448305b2dc121e1d
kzk0829 <kzk0829kop@gmail.com>
2019-09-15 10:49:12 +0900

    add 14

---
+++ 10-19/14.py
@@ -0,0 +1,15 @@
+# 14. 先頭からN行を出力
+# 自然数Nをコマンドライン引数などの手段で受け取り，入力のうち先頭のN行だけを表示せよ．確認にはheadコマンドを用いよ．
+
+import sys
+
+argv = sys.argv
+n = int(argv[1])
+
+with open("hightemp.txt") as f:
+    for i, l in enumerate(f):
+        print(l, end="")
+        if i == n-1:
+            break
+
+# head -n 5 hightemp.txt
