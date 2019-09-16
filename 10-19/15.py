7d28c7d01d5c41f1369a2a06fed5ad4fb6074132
kzk0829 <kzk0829kop@gmail.com>
2019-09-16 16:17:34 +0900

    add 15

---
+++ 10-19/15.py
@@ -0,0 +1,13 @@
+# 15. 末尾のN行を出力
+# 自然数Nをコマンドライン引数などの手段で受け取り，入力のうち末尾のN行だけを表示せよ．確認にはtailコマンドを用いよ．
+
+import sys
+
+argv = sys.argv
+n = int(argv[1])
+
+with open("hightemp.txt") as f:
+    for l in range(n):
+        print(next(f), end="")
+
+# tail -n 2 hightemp.txt
