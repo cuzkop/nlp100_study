6dfb0c98e10a42a040f31e3b1c7855474d297b7f
kzk0829 <kzk0829kop@gmail.com>
2019-09-19 23:23:04 +0900

    add 17

--- 10-19/17.py
+++ 10-19/17.py
@@ -2,5 +2,14 @@
 # 1列目の文字列の種類（異なる文字列の集合）を求めよ．確認にはsort, uniqコマンドを用いよ．
 
 with open("tmp/hightemp.txt", "r") as f:
-    print(f.readlines())
-    pass
+    files = f.readlines()
+
+
+s = set()
+for l in files:
+    lst = l.split("\t")
+    s.add(lst[0])
+
+print(s)
+
+# cut -f 1 tmp/hightemp.txt | sort | uniq
