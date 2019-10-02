04d477a2d5395865de17685ff44f2a38ff53252a
kzk0829 <kzk0829kop@gmail.com>
2019-10-02 23:16:17 +0900

    add 33

---
+++ 30-39/33.py
@@ -0,0 +1,29 @@
+# 33. サ変名詞
+# サ変接続の名詞をすべて抽出せよ．
+
+import MeCab
+
+with open("tmp/neko.txt", "r") as f, open("tmp/neko.txt.mecab", "w") as mf:
+    m = MeCab.Tagger()
+    mf.write(m.parse(f.read()))
+
+def createList():
+    with open("tmp/neko.txt.mecab", "r") as mf:
+        l = []
+        for m in mf:
+            word = m.split("\t")
+            if len(word) < 2:
+                continue
+            exp = word[1].split(",")
+            d = {"surface" : word[0], "base" : exp[6], "pos" : exp[0], "pos1" : exp[1]}
+            l.append(d)
+            if exp[1] == "句点":
+                yield l
+                l = []
+
+
+l = createList()
+for ls in l:
+    for d in ls:
+        if d["pos1"] == "サ変接続" and d["pos"] == "名詞":
+            print(d["surface"])
