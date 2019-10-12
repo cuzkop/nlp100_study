2b7116cb99a952a97a8f147aa62cbfd4e59f5d27
kzk0829 <kzk0829kop@gmail.com>
2019-10-12 16:02:15 +0900

    add 43

---
+++ 40-49/43.py
@@ -0,0 +1,103 @@
+# 43. 名詞を含む文節が動詞を含む文節に係るものを抽出
+# 名詞を含む文節が，動詞を含む文節に係るとき，これらをタブ区切り形式で抽出せよ．ただし，句読点などの記号は出力しないようにせよ．
+
+import CaboCha
+import sys
+
+with open("tmp/neko.txt") as read_file, open("tmp/neko.txt.cabocha", mode='w') as write_file:
+    cabocha = CaboCha.Parser()
+    for l in read_file:
+        write_file.write(
+            cabocha.parse(l).toString(CaboCha.FORMAT_LATTICE)
+        )
+
+class Morph:
+
+    def __init__(self, surface, base, pos, pos1):
+        self.surface = surface
+        self.base = base
+        self.pos = pos
+        self.pos1 = pos1
+
+    def __str__(self):
+        return "surface[{}]\tbase[{}]\tpos[{}]\tpos1[{}]".format(self.surface, self.base, self.pos, self.pos1)
+
+class Chunk:
+
+    def __init__(self):
+        self.morphs = []
+        self.dst = 0
+        self.srcs = []
+
+    def __str__(self):
+        surface = ""
+        for morph in self.morphs:
+            surface += morph.surface
+        return "{} 係り先: dst[{}]".format(surface, self.dst)
+
+    def getSurface(self):
+        surface = ""
+        for morph in self.morphs:
+            surface += morph.surface
+        return surface
+
+    def getPos(self, pos):
+        for morph in self.morphs:
+            if morph.pos == pos:
+                return True
+
+        return False
+
+
+
+def createList():
+    with open("tmp/neko.txt.cabocha", "r") as mf:
+
+        chunks = {}
+        i = -1
+
+        for l in mf:
+            if l == "EOS\n":
+                if len(chunks) > 0:
+                    sorted_tuple = sorted(chunks.items(), key=lambda x: x[0])
+                    yield list(zip(*sorted_tuple))[1]
+                    chunks.clear()
+                else:
+                    yield []
+
+            elif l[0] == "*":
+                row = l.split(" ")
+                i = int(row[1])
+                dst = int(row[2].rstrip("D"))
+
+                if i not in chunks:
+                    chunks[i] = Chunk()
+
+                chunks[i].dst = dst
+
+                if dst != -1:
+                    if dst not in chunks:
+                        chunks[dst] = Chunk()
+                    chunks[dst].srcs.append(i)
+
+            else:
+                c = l.split("\t")
+                s = c[1].split(",")
+
+                chunks[i].morphs.append(Morph(c[0], s[6], s[0], s[1]))
+
+        raise StopIteration
+
+
+for i, c in enumerate(createList(), 1):
+    for chunk in c:
+        if chunk.dst == -1:
+            continue
+        if chunk.getPos("名詞") and c[chunk.dst].getPos("動詞"):
+            src = chunk.getSurface()
+            dst = c[chunk.dst].getSurface()
+            
+            if src == "" or dst == "":
+                continue
+
+            print("{}\t{}".format(src, dst))
