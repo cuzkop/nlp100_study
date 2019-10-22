82675ca9fbbfcb846c258bbee3988bd18cec82d5
kzk0829 <kzk0829kop@gmail.com>
2019-10-22 18:44:25 +0900

    add 52

--- 50-59/52.py
+++ 50-59/52.py
@@ -2,7 +2,6 @@
 # 51の出力を入力として受け取り，Porterのステミングアルゴリズムを適用し，単語と語幹をタブ区切り形式で出力せよ． Pythonでは，Porterのステミングアルゴリズムの実装としてstemmingモジュールを利用するとよい．
 
 import re
-import sys
 from stemming.porter2 import stem
 
 def get_sentence():
