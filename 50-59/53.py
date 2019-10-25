5e9abf6ef73477e22c86f84d5cc3f50e99c39c01
kzk0829 <kzk0829kop@gmail.com>
2019-10-25 22:02:41 +0900

    add 53

--- 50-59/53.py
+++ 50-59/53.py
@@ -1,3 +1,9 @@
 # 53. Tokenization
 # Stanford Core NLPを用い，入力テキストの解析結果をXML形式で得よ．また，このXMLファイルを読み込み，入力テキストを1行1単語の形式で出力せよ．
 
+import xml.etree.ElementTree as ET
+
+tree = ET.parse("tmp/nlp.txt.xml")
+
+for r in tree.iter("word"):
+    print(r.text)
