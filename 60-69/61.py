e5fd71fe663b44a2112c09a6e56dc89d1ce9c1a5
kzk0829 <kzk0829kop@gmail.com>
2019-11-04 16:27:58 +0900

    add 61

--- 60-69/61.py
+++ 60-69/61.py
@@ -1,2 +1,16 @@
 # 61. KVSの検索
 # 60で構築したデータベースを用い，特定の（指定された）アーティストの活動場所を取得せよ．
+
+import redis
+import sys
+
+argv = sys.argv
+artist_name = str(argv[1])
+redis = redis.Redis(host="localhost", port=6379, db=0)
+
+place = redis.get(artist_name)
+
+if place != None:
+    print(str(place, encoding="utf-8"))
+else:
+    print("None")
