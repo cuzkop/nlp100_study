2b49e99f9ccd3d6de75093b4d4b3de056a04f9db
kzk0829 <kzk0829kop@gmail.com>
2019-11-08 00:26:09 +0900

    add 63

--- 60-69/63.py
+++ 60-69/63.py
@@ -10,24 +10,18 @@ artist_name = str(argv[1])
 
 redis = redis.Redis(host="localhost", port=6379, db=0)
 
-with open("tmp/artist.json", "r") as file:
-    cnt = 0
-    for f in file:
-        line = json.loads(f)
-        # print(type(str(line.get("tags", ""))))
-        redis.set(line["name"], str(line.get("tags", "")))
-        if cnt == 30:
-            break
-        cnt+=1
+# ここは重すぎるので最初のみ
+# with open("tmp/artist.json", "r") as file:
+#     for f in file:
+#         line = json.loads(f)
+#         redis.set(line["name"], str(line.get("tags", "")))
 
 string_tags = redis.get(artist_name)
+if str(string_tags, encoding="utf-8") == "":
+    print("None")
+    sys.exit()
+
 tags = json.loads(str(string_tags, encoding="utf-8").replace("'", '"'))
-print(type(tags))
 
-
-# if "tags" in line:
-#             for tag in line["tags"]:
-#                 print(tag["count"])
-#         cnt += 1
-#         if cnt == 30:
-#             sys.exit()
+for tag in tags:
+    print("value : {}, count : {}".format(tag["value"], tag["count"]))
