82deea56bd0ac1645eb902a98d04c78446abd1a6
kzk0829 <kzk0829kop@gmail.com>
2019-11-09 15:01:47 +0900

    add 65

---
+++ 60-69/65.py
@@ -0,0 +1,13 @@
+# 65. MongoDBの検索
+# MongoDBのインタラクティブシェルを用いて，"Queen"というアーティストに関する情報を取得せよ．さらに，これと同様の処理を行うプログラムを実装せよ．
+
+import json
+from pymongo import MongoClient 
+
+client = MongoClient("localhost", 27017)
+db = client.artist_db
+collection = db.artist
+
+for queen in collection.find({'name':'Queen'}):
+    for q in queen.values():
+        print(q)
