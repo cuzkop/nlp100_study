151a90263c8874746b39be9c8ec2541ed3ec9ae2
kzk0829 <kzk0829kop@gmail.com>
2019-11-10 18:35:32 +0900

    add 66

---
+++ 60-69/66.py
@@ -0,0 +1,10 @@
+# 66. 検索件数の取得
+# MongoDBのインタラクティブシェルを用いて，活動場所が「Japan」となっているアーティスト数を求めよ．
+
+# シェル上での操作を以下に記載
+
+print("Please reading 66.py file.")
+
+# $ mongo
+# $ use artist_db
+# $ db.artist.find({'area': 'Japan'}).count()
