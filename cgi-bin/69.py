100e80584589bab870a2f54e04ca696027370b2d
kzk0829 <kzk0829kop@gmail.com>
2019-11-14 16:25:48 +0900

    条件作成まで

--- cgi-bin/69.py
+++ cgi-bin/69.py
@@ -4,10 +4,51 @@
 import cgi
 import cgitb
 import os
+import sys
 
 cgitb.enable()
 
+name = ""
+alias = ""
+tag = ""
+condition = {}
 form = cgi.FieldStorage()
+
+print ("Content-Type: text/html")
+print()
+
+# 各検索項目の抽出
+if "name" in form:
+    name = form["name"].value
+
+if "alias" in form:
+    alias = form["alias"].value
+
+if "tag" in form:
+    tag = form["tag"].value
+
+
+# 検索項目に応じた条件の作成
+if name != "":
+    condition = {"name" : name}
+
+if alias != "":
+    if len(condition) < 1:
+        condition = {"alias" : alias}
+    else:
+        condition = {"$and" : [condition, {"alias" : alias}]}
+
+if tag != "":
+    if len(condition) < 1:
+        condition = {"tag" : tag}
+    elif "$and" in condition:
+        condition["$and"].append({"tag" : tag})
+    else:
+        condition = {"$and" : [condition, {"tag" : tag}]}
+
+print(condition)
+
+sys.exit()
 
 print ("Content-Type: text/html")
 print()
@@ -16,5 +57,4 @@ print()
 
 print("<html><body>")
 print("<h1>TEST</h1>")
-print(form["name"].value)
 print("</body></html>")
