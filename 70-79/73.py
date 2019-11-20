017f514d8979e8f3dfc720128dbf7ea093346e21
kzk0829 <kzk0829kop@gmail.com>
2019-11-20 22:53:36 +0900

    73 途中

--- 70-79/73.py
+++ 70-79/73.py
@@ -25,20 +25,40 @@ def load_feature() -> dict:
     with open("tmp/features.txt", mode="r", encoding="utf8", errors="ignore") as feature:
         return {f.strip(): i for i, f in enumerate(feature, start=1)}
 
-def create_train_data(sentiments: list, feature_dict: dict):
+def create_train_data(sentiments: list, feature_dict: dict) -> list:
     train_x = np.zeros([len(sentiments), len(feature_dict) + 1], dtype=np.float64)
     train_y = np.zeros(len(sentiments), dtype=np.float64)
-    
+
     for i, sentiment in enumerate(sentiments):
         train_x[i] = extract_feature(sentiment[3:], feature_dict)
-        sys.exit()
+        
+        if sentiment[0:2] == "+1":
+            train_y[i] = 1
 
-    return "a", "b"
+    return train_x, train_y
 
-def extract_feature(data, feature_dict):
-    
+def extract_feature(data: str, feature_dict: dict) -> list:
+    train_x = np.zeros(len(feature_dict) + 1, dtype=np.float64)
+    train_x[0] = 1
+
+    for word in data.split(' '):
+        word = word.strip()
+        
+        if is_stopword(word):
+            continue
+
+        word = stem(word)
+
+        try:
+            train_x[feature_dict[word]] = 1
+        except:
+            pass
+
+    return train_x
+
 
 feature_dict = load_feature()
 
 with open("tmp/sentiment.txt", mode="r", encoding="utf8", errors="ignore") as sentiment:
     x_train, y_train = create_train_data(list(sentiment), feature_dict)
+    print(len(x_train), len(y_train))
