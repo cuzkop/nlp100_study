0e75ea40816c5ea763fb9d25b932585564345fce
kzk0829 <kzk0829kop@gmail.com>
2019-11-25 23:29:26 +0900

    add 70 retry

---
+++ 70-79/70-retry.py
@@ -0,0 +1,40 @@
+# 第8章: 機械学習
+# 本章では，Bo Pang氏とLillian Lee氏が公開している
+# Movie Review Dataのsentence polarity dataset v1.0を用い，
+# 文を肯定的（ポジティブ）もしくは否定的（ネガティブ）に分類するタスク（極性分析）に取り組む．
+
+# 70. データの入手・整形
+# 文に関する極性分析の正解データを用い，以下の要領で正解データ（sentiment.txt）を作成せよ．
+
+# 1.rt-polarity.posの各行の先頭に"+1 "という文字列を追加する（極性ラベル"+1"とスペースに続けて肯定的な文の内容が続く）
+# 2.rt-polarity.negの各行の先頭に"-1 "という文字列を追加する（極性ラベル"-1"とスペースに続けて否定的な文の内容が続く）
+# 3.上述1と2の内容を結合（concatenate）し，行をランダムに並び替える
+# sentiment.txtを作成したら，正例（肯定的な文）の数と負例（否定的な文）の数を確認せよ．
+
+import random
+import sys
+
+result = []
+
+with open("tmp/rt-polaritydata/rt-polarity.pos", mode="r", encoding="utf8", errors="ignore") as pos:
+    result.extend(["+1 {}".format(sentence.strip()) for sentence in pos])
+
+with open("tmp/rt-polaritydata/rt-polarity.neg", mode="r", encoding="utf8", errors="ignore") as neg:
+    result.extend(["-1 {}".format(sentence.strip()) for sentence in neg])
+
+
+random.shuffle(result)
+
+with open("tmp/sentiment-retry.txt", mode="w", encoding="utf8", errors="ignore") as sentiment:
+    sentiment.write("\n".join(result))
+
+with open("tmp/sentiment-retry.txt", mode="r", encoding="utf8", errors="ignore") as sentiment:
+    pos = 0
+    neg = 0
+    for line in sentiment:
+        if line.startswith("+1"):
+            pos += 1
+        if line.startswith("-1"):
+            neg += 1
+
+print("positive sentences : {} \nnegative sentences : {}".format(pos, neg))
